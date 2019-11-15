bd1a96b27cbeb6c9361bde6480267e3a232d1f9a
Joseph LaRue <JLaRue@wolfgreenfield.com>
2019-11-15 12:09:06 -0500

    added initial project

---
+++ src/IdentityAdmin/IdentityAdmin/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace IdentityAdmin.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public IndexModel(ILogger<IndexModel> logger, RoleManager<IdentityRole> roleManager)
+        {
+            _logger = logger;
+            _roleManager = roleManager;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
